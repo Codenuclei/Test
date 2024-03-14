e6c653c684372ac4ca9016edf1430d91e3570e30
Codenuclei <160272247+Codenuclei@users.noreply.github.com>
2024-03-14 17:11:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
-const app = express();
-const port = 8080;
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-app.get('/', (req, res) => {
-  res.send('Hello, world!');
-});
-
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
